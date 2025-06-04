e74fe189361c3b8626b5de42d364dcde67e33804
bahaa <pixelatedbahaa@gmail.com>
2025-06-04 09:36:45 +0100

    Update retro.sh

--- retro.sh
+++ retro.sh
@@ -1,1 +1,4 @@
-hi
+mkdir -p /home/.config/retroarch/states/ParaLLEl\ N64 /home/.config/retroarch/saves/ParaLLEl\ N64
+cp Super\ Mario\ 64\ \(\USA\)\.state /home/.config/retroarch/states/ParaLLEl\ N64\/
+cp Super\ Mario\ 64\ \(\USA\)\.srm /home/.config/retroarch/saves/ParaLLEl\ N64\/
+cp retroarch.cfg /home/.config/retroarch/
